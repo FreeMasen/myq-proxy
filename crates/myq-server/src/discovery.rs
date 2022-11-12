b97f8f538da0c210e193ba5b74ccbe78f15a034a
Robert Masen <r.f.masen@gmail.com>
2022-11-12 16:36:22 -0600

    initial features complete

--- crates/myq-server/src/discovery.rs
+++ crates/myq-server/src/discovery.rs
@@ -1,6 +1,6 @@
 use std::net::Ipv4Addr;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use tokio::net::UdpSocket;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -19,29 +19,36 @@ const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(239, 255, 255, 250);
 const MULTICAST_PORT: u16 = 1919;
 
 pub async fn discovery_task(ip: Ipv4Addr, port: u16) {
+    log::debug!("starting discovery task {ip}:{port}");
     let udp = get_sock().await;
     loop {
-        let mut buf = vec![0u8;65535];
+        let mut buf = vec![0u8; 65535];
         let (ct, who) = udp.recv_from(&mut buf).await.unwrap();
-        log::debug!("req({ct}, {who}): {:?}", String::from_utf8_lossy(&buf[..ct]));
-        if let Ok(msg) = serde_json::from_slice::<QueryMessage>(&buf[..ct]).map_err(|e| log::debug!("bad query: {e}")) {
+        log::debug!(
+            "req({ct}, {who}): {:?}",
+            String::from_utf8_lossy(&buf[..ct])
+        );
+        if let Ok(msg) = serde_json::from_slice::<QueryMessage>(&buf[..ct])
+            .map_err(|e| log::debug!("bad query: {e}"))
+        {
             if msg.target == "myq-proxy" {
                 log::info!("handling disovery request from {}", msg.source);
-                udp.send_to(&serde_json::to_vec(&QueryReply {
-                    ip, port
-                }).unwrap(), who).await.unwrap();
+                udp.send_to(&serde_json::to_vec(&QueryReply { ip, port }).unwrap(), who)
+                    .await
+                    .unwrap();
             }
         }
     }
 }
 
 async fn get_sock() -> UdpSocket {
+    use socket2::{Domain, SockAddr, Socket, Type};
     use std::net::{SocketAddr, SocketAddrV4};
-    use socket2::{Socket, Domain, Type, SockAddr};
     let udp = Socket::new(Domain::IPV4, Type::DGRAM, None).unwrap();
     udp.set_reuse_port(true).unwrap();
     udp.set_reuse_address(true).unwrap();
-    udp.join_multicast_v4(&MULTICAST_ADDR, &Ipv4Addr::new(0, 0, 0, 0)).unwrap();
+    udp.join_multicast_v4(&MULTICAST_ADDR, &Ipv4Addr::new(0, 0, 0, 0))
+        .unwrap();
     udp.set_multicast_loop_v4(false).unwrap();
     let sock_addr = SocketAddrV4::new(MULTICAST_ADDR, MULTICAST_PORT);
     let sock_addr = SocketAddr::V4(sock_addr);
@@ -54,14 +61,14 @@ async fn get_sock() -> UdpSocket {
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[tokio::test]
     async fn replies() {
-        let target_addr = Ipv4Addr::new(192,168,1,200);
+        let target_addr = Ipv4Addr::new(192, 168, 1, 200);
         let target_port = 9999u16;
         tokio::task::spawn(discovery_task(target_addr, target_port));
         tokio::task::yield_now().await;
-        let addr = Ipv4Addr::new(0,0,0,0);
+        let addr = Ipv4Addr::new(0, 0, 0, 0);
         let udp = UdpSocket::bind((addr, 0)).await.unwrap();
         udp.join_multicast_v4(MULTICAST_ADDR, addr).unwrap();
         let query = QueryMessage {
@@ -69,9 +76,12 @@ mod test {
             target: "myq-proxy".to_string(),
         };
         let query_bytes = serde_json::to_vec(&query).unwrap();
-        let send_ct = udp.send_to(&query_bytes, (MULTICAST_ADDR, MULTICAST_PORT)).await.unwrap();
+        let send_ct = udp
+            .send_to(&query_bytes, (MULTICAST_ADDR, MULTICAST_PORT))
+            .await
+            .unwrap();
         assert_eq!(send_ct, query_bytes.len());
-        let mut buf = vec![0u8;4096];
+        let mut buf = vec![0u8; 4096];
         let (ct, _who) = udp.recv_from(&mut buf).await.unwrap();
         let resp: QueryReply = serde_json::from_slice(&buf[..ct]).unwrap();
         assert_eq!(target_addr, resp.ip);
--- crates/myq-server/src/main.rs
+++ crates/myq-server/src/main.rs
@@ -1,21 +1,29 @@
-use std::fs::File;
+use std::{convert::Infallible, fs::File, net::IpAddr};
 
 use futures_util::{
     stream::{SplitSink, SplitStream},
-    SinkExt, Stream, StreamExt, TryFutureExt,
+    SinkExt, StreamExt,
 };
-use myq_proxy::{Config, Device, DeviceStateActor, DeviceStateHandle, StateChange};
+use myq_proxy::{
+    Config, Device, DeviceStateActor, DeviceStateHandle, DeviceType, GarageDoorState, LampState,
+    StateChange,
+};
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use socket2::Socket;
+use tokio::sync::mpsc::{self, UnboundedSender};
 use warp::{
+    hyper::StatusCode,
+    reply::Response,
     ws::{Message, WebSocket, Ws},
-    Filter,
+    Filter, Reply,
 };
 
 mod discovery;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    let ip = find_my_ip();
     let config_dir = dirs::config_dir().unwrap_or_else(|| {
         panic!("config dir is required");
     });
@@ -30,20 +38,49 @@ async fn main() {
     let with_handle = warp::any().map(move || handle.clone());
     let event_stream = warp::path("event-stream")
         .and(warp::ws())
-        .and(with_handle)
-        .map(|ws: Ws, handle: DeviceStateHandle| {
-            ws.on_upgrade(|sock| async move {
-                let (tx, rx) = sock.split();
-                let tx = wrap_ws_in_ch(tx);
-                wrap_receiver(rx, tx.clone(), handle.clone());
-                forward_event_stream(handle, tx);
-            })
+        .and(with_handle.clone())
+        .map(|ws: Ws, handle: DeviceStateHandle| ws.on_upgrade(|sock| on_ws_upgrade(sock, handle)));
+    let get_devices = warp::path("devices")
+        .and(warp::get())
+        .and(with_handle.clone())
+        .and_then(|handle: DeviceStateHandle| async move {
+            Result::<_, Infallible>::Ok(get_devices(&handle).await.as_reply())
         });
-    let all = event_stream.with(warp::log("myq-server"));
-    tokio::task::spawn(async move {
-        warp::serve(all).run(([127, 0, 0, 1], 6578)).await;
-    });
+    let get_device = warp::path!("device" / String)
+        .and(warp::get())
+        .and(with_handle.clone())
+        .and_then(|id: String, handle: DeviceStateHandle| async move {
+            Result::<_, Infallible>::Ok(get_device(id, &handle).await.as_reply())
+        });
+    let execute_command = warp::path!("command" / String)
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_handle.clone())
+        .and_then(
+            |id: String, command: Command, handle: DeviceStateHandle| async move {
+                Result::<_, Infallible>::Ok(execute_command(id, command, &handle).await.as_reply())
+            },
+        );
+    let all = event_stream
+        .or(get_devices)
+        .or(get_device)
+        .or(execute_command)
+        .with(warp::log("myq_server"));
+    let (addr, server) = warp::serve(all).bind_ephemeral((ip, 0));
+    let ip = match addr.ip() {
+        std::net::IpAddr::V4(inner) => inner,
+        std::net::IpAddr::V6(_) => panic!("v4 address not yet supported"),
+    };
+    tokio::task::spawn(discovery::discovery_task(ip, addr.port()));
+    tokio::task::spawn(server);
     actor.run().await.unwrap();
+}
+
+async fn on_ws_upgrade(sock: WebSocket, handle: DeviceStateHandle) {
+    let (tx, rx) = sock.split();
+    let tx = wrap_ws_in_ch(tx);
+    wrap_receiver(rx, tx.clone(), handle.clone());
+    forward_event_stream(handle, tx);
 }
 
 /// Forward all message through an UnboundedReceiver to allow for cloning the tx into
@@ -66,26 +103,38 @@ fn wrap_receiver(
 ) {
     tokio::task::spawn(async move {
         while let Some(msg) = rx.next().await {
-            if let Ok(msg) = msg.map_err(|e| log::error!("received error {e}")) {
-                if let Ok(msg) = serde_json::from_slice::<IncomingMsg>(msg.as_bytes())
-                    .map_err(|e| log::error!("Error deserializing message {e}"))
-                {
-                    match msg {
-                        IncomingMsg::GetDevice(_) => {
-                            let message = handle
-                                .get_devices()
-                                .await
-                                .map(|devs| OutgoingMsg::GetDevices(devs))
-                                .unwrap_or_else(|e| OutgoingMsg::Error(format!("{e}")));
-                            tx.send(message)
-                                .map_err(|e| log::error!("Error sending to wrapped ws: {e:?}"))
-                                .ok();
-                        }
-                    }
-                }
-            }
+            let msg = process_incoming(msg, &handle).await.unwrap_or_else(|e| e);
+            tx.send(msg)
+                .map_err(|e| log::error!("Error sending to wrapped ws: {e:?}"))
+                .ok();
         }
     });
+}
+
+async fn process_incoming(
+    incoming: Result<Message, warp::Error>,
+    handle: &DeviceStateHandle,
+) -> Result<OutgoingMsg, OutgoingMsg> {
+    log::trace!("incoming msg: {incoming:?}");
+    let msg = incoming.map_err(|e| {
+        log::error!("received error {e}");
+        e
+    })?;
+    let msg = serde_json::from_slice::<IncomingMsg>(msg.as_bytes()).map_err(|e| {
+        log::error!("Error deserializing message {e}");
+        e
+    })?;
+    Ok(process_incoming_message(msg, handle).await)
+}
+
+async fn process_incoming_message(msg: IncomingMsg, handle: &DeviceStateHandle) -> OutgoingMsg {
+    match msg {
+        IncomingMsg::GetDevices => get_devices(handle).await,
+        IncomingMsg::GetDevice { device_id } => get_device(device_id, handle).await,
+        IncomingMsg::ExecuteCommand { device_id, command } => {
+            execute_command(device_id, command, handle).await
+        }
+    }
 }
 
 fn forward_event_stream(handle: DeviceStateHandle, tx: UnboundedSender<OutgoingMsg>) {
@@ -100,7 +149,7 @@ fn forward_event_stream(handle: DeviceStateHandle, tx: UnboundedSender<OutgoingM
                 .await
                 .map_err(|e| log::error!("error receiving {e}"))
             {
-                tx.send(OutgoingMsg::StateChange(msg))
+                tx.send(OutgoingMsg::StateChange { change: msg })
                     .map_err(|e| {
                         log::error!("Error sending msg: {e}");
                     })
@@ -110,19 +159,228 @@ fn forward_event_stream(handle: DeviceStateHandle, tx: UnboundedSender<OutgoingM
     });
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "data")]
-enum IncomingMsg {
-    GetDevice(GetDevices),
+async fn get_devices(handle: &DeviceStateHandle) -> OutgoingMsg {
+    handle
+        .get_devices()
+        .await
+        .map(|devices| OutgoingMsg::GetDevices { devices })
+        .unwrap_or_else(|e| OutgoingMsg::Error {
+            err: format!("{e}"),
+        })
+}
+
+async fn get_device(device_id: String, handle: &DeviceStateHandle) -> OutgoingMsg {
+    match handle.get_device(device_id).await {
+        Ok(Some(device)) => OutgoingMsg::GetDevice { device },
+        Ok(None) => OutgoingMsg::Error {
+            err: "Device Not Found".into(),
+        },
+        Err(e) => OutgoingMsg::Error {
+            err: format!("{e}"),
+        },
+    }
+}
+
+async fn execute_command(id: String, command: Command, handle: &DeviceStateHandle) -> OutgoingMsg {
+    try_execute_command(id, command, handle.clone())
+        .await
+        .map(|_| OutgoingMsg::Success)
+        .unwrap_or_else(|err| OutgoingMsg::Error { err })
+}
+
+async fn try_execute_command(
+    id: String,
+    command: Command,
+    handle: DeviceStateHandle,
+) -> Result<(), String> {
+    let device = handle
+        .get_device(id.clone())
+        .await
+        .map_err(|e| format!("{e:?}"))?
+        .ok_or_else(|| format!("Unknown Device {id}"))?;
+    match (device.get_type_state(), &command) {
+        (Some(DeviceType::Lamp(LampState::Off)), Command::On)
+        | (Some(DeviceType::Lamp(LampState::On)), Command::Off) => {
+            handle
+                .toggle_device(id)
+                .await
+                .map_err(|e| format!("{e:?}"))
+                .ok();
+        }
+        (Some(DeviceType::GarageDoor(GarageDoorState::Open)), Command::Close)
+        | (Some(DeviceType::GarageDoor(GarageDoorState::Closed)), Command::Open) => {
+            handle
+                .toggle_device(id)
+                .await
+                .map_err(|e| format!("{e:?}"))
+                .ok();
+        }
+        (None, _) => return Err(format!("Device State not found")),
+        _ => {
+            return Err(format!(
+                "Invalid command for {}: {:?}",
+                device.device_family, command
+            ))
+        }
+    }
+    Ok(())
+}
+
+impl OutgoingMsg {
+    fn as_reply(&self) -> impl Reply {
+        let status = if matches!(self, OutgoingMsg::Error { .. }) {
+            StatusCode::INTERNAL_SERVER_ERROR
+        } else {
+            StatusCode::OK
+        };
+        warp::reply::with_status(warp::reply::json(&self), status)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct GetDevices;
+#[serde(tag = "type")]
+enum IncomingMsg {
+    GetDevices,
+    GetDevice { device_id: String },
+    ExecuteCommand { device_id: String, command: Command },
+}
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "data")]
+enum Command {
+    On,
+    Off,
+    Open,
+    Close,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
 enum OutgoingMsg {
-    GetDevices(Vec<Device>),
-    StateChange(StateChange),
-    Error(String),
+    GetDevices { devices: Vec<Device> },
+    GetDevice { device: Device },
+    StateChange { change: StateChange },
+    Success,
+    Error { err: String },
+}
+
+impl From<warp::Error> for OutgoingMsg {
+    fn from(e: warp::Error) -> Self {
+        Self::Error {
+            err: format!("{e}"),
+        }
+    }
+}
+
+impl From<serde_json::Error> for OutgoingMsg {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Error {
+            err: format!("{e}"),
+        }
+    }
+}
+
+fn find_my_ip() -> IpAddr {
+    use socket2::{Domain, SockAddr, Type};
+    use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
+    let sock_addr = SocketAddrV4::new(Ipv4Addr::new(192, 168, 1, 1), 0);
+    let sock_addr = SocketAddr::V4(sock_addr);
+    let sock_addr = SockAddr::from(sock_addr);
+    let udp = Socket::new(Domain::IPV4, Type::DGRAM, None).unwrap();
+    udp.connect(&sock_addr).unwrap();
+    let peer_addr = udp.local_addr().unwrap();
+    IpAddr::V4(*peer_addr.as_socket_ipv4().unwrap().ip())
+}
+
+#[cfg(test)]
+mod test {
+    use myq_proxy::DeviceState;
+    use uuid::Uuid;
+
+    use super::*;
+
+    #[test]
+    fn incoming_msg_ser() {
+        let incoming = vec![
+            IncomingMsg::GetDevices,
+            IncomingMsg::GetDevice {
+                device_id: String::new(),
+            },
+            IncomingMsg::ExecuteCommand {
+                command: Command::Close,
+                device_id: String::new(),
+            },
+        ];
+        insta::assert_json_snapshot!(incoming);
+    }
+    #[test]
+    fn outgoing_msg_ser() {
+        let outgoing = vec![
+            OutgoingMsg::GetDevices { devices: vec![] },
+            OutgoingMsg::GetDevice {
+                device: Device {
+                    account_id: Uuid::from_u128(0),
+                    created_date: None,
+                    device_family: String::new(),
+                    device_model: String::new(),
+                    device_platform: String::new(),
+                    device_type: String::new(),
+                    href: String::new(),
+                    name: String::new(),
+                    parent_device_id: String::new(),
+                    serial_number: String::new(),
+                    state: DeviceState {
+                        attached_work_light_error_present: false,
+                        aux_relay_behavior: None,
+                        aux_relay_delay: None,
+                        close: None,
+                        command_channel_report_status: Default::default(),
+                        control_from_browser: Default::default(),
+                        door_ajar_interval: Default::default(),
+                        door_state: Some(GarageDoorState::Closed),
+                        dps_low_battery_mode: Default::default(),
+                        firmware_version: Default::default(),
+                        gdo_lock_connected: Default::default(),
+                        homekit_capable: Default::default(),
+                        homekit_enabled: Default::default(),
+                        invalid_credential_window: Default::default(),
+                        invalid_shutout_period: Default::default(),
+                        is_unattended_close_allowed: Default::default(),
+                        is_unattended_open_allowed: Default::default(),
+                        lamp_state: Some(LampState::Off),
+                        lamp_subtype: Default::default(),
+                        last_event: Default::default(),
+                        last_status: Default::default(),
+                        last_update: Default::default(),
+                        learn: Default::default(),
+                        learn_mode: Default::default(),
+                        light_state: Default::default(),
+                        links: Default::default(),
+                        max_invalid_attempts: Default::default(),
+                        online: Default::default(),
+                        online_change_time: Default::default(),
+                        open: Default::default(),
+                        passthrough_interval: Default::default(),
+                        pending_bootload_abandoned: Default::default(),
+                        physical_devices: Default::default(),
+                        report_ajar: Default::default(),
+                        report_forced: Default::default(),
+                        rex_fires_door: Default::default(),
+                        servers: Default::default(),
+                        updated_date: Default::default(),
+                        use_aux_relay: Default::default(),
+                    },
+                },
+            },
+            OutgoingMsg::StateChange {
+                change: StateChange::DeviceAdded {
+                    device_id: String::new(),
+                    device_type: DeviceType::GarageDoor(GarageDoorState::Closed),
+                },
+            },
+            OutgoingMsg::Success,
+            OutgoingMsg::Error { err: String::new() },
+        ];
+        insta::assert_json_snapshot!(outgoing);
+    }
 }
